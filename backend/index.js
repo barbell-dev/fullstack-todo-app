983eb18b5311d23858ddef081be96b13112ed638
Varun Dunna <varun.dunna2524@gmail.com>
2024-09-21 22:10:21 +0530

    test

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const app = express();
 const port = process.env.PORT || 8080;
 app.use(
   cors({
-    origin: ["https://fullstack-todo-app-two.vercel.app/"],
+    origin: ["https://fullstack-todo-app-two.vercel.app"],
     credentials: true,
     acc,
   })
